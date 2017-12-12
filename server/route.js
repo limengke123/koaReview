e43cb67987d6adb06cd6d228c4b029860df89d45
limengke <752140235@qq.com>
2017-12-12 17:07:51 +0800

    明白路径了

--- server/route.js
+++ server/route.js
@@ -4,8 +4,8 @@
 const Router = require('koa-router')()
 const dir = require('./api/dir')
 const article = require('./api/article')
-// const fs = require('fs')
-// const path = require('path')
+ const fs = require('fs')
+ const path = require('path')
 module.exports = function(app){
     Router.use('/dir',dir.routes(),dir.allowedMethods())
     Router.use('/article',article.routes(),article.allowedMethods())
@@ -15,10 +15,13 @@ module.exports = function(app){
             data:"first time be here"
         }
     })
-    // Router.get('/fk',(ctx,next)=>{
-    //     ctx.body = fs.readFileSync(path.resolve(__dirname,'../view/index.html'))
-    //     //ctx.body = 2
-    // })
+    Router.get('/fk',(ctx,next)=>{
+        let paths = path.resolve(__dirname,'../view/index.html')
+        console.log(ctx.url)
+        ctx.type = "text/html"
+        ctx.body = fs.createReadStream(paths)
+        //ctx.body = 2
+    })
     Router.get('/*',(ctx,next)=>{
         // ctx.body = "hi man,it seems that you got lost here!"
         ctx.render('index',{data:'fk'})
