b9058c598d221c9b516fc970e731b9ed81dd29b7
limengke <752140235@qq.com>
2018-01-02 15:49:51 +0800

    图片爬取、保存、显示 例子

--- server/contoller/spider/index.js
+++ server/contoller/spider/index.js
@@ -1,15 +1,49 @@
 /**
  * Created by li on 2017/12/29 16:06.
  */
+const {GET,get$} = require('../../util')
+const {spiderConfig} = require('../../config')
+const {uri,decodeType} = spiderConfig.dytt
 const sp = async (ctx, next) => {
     ctx.body = "spider"
 }
 
 const dytt = async (ctx,next) => {
-    ctx.body =  "dytt"
+    try{
+        const $ = await get$(uri,decodeType)
+        const data = $(".title_all").find('p').text()
+        console.log(data)
+        ctx.body =  "dytt"
+    } catch (e) {
+        console.log(e)
+        ctx.boyd = "Error"
+    }
+}
+
+const download = async (ctx,next) => {
+    const buffer = await GET("http://dev.zwu.flyread.local/image/holiday/bgTmp_zwu.png",void 0,true)
+    console.log(buffer)
+    const picPath = require('path').resolve(__dirname,'../../../logs/img.jpeg')
+    console.log(picPath)
+    require('fs').writeFile(picPath,buffer,(e)=>{
+        if(!e){
+            ctx.body = "download is ok"
+        }
+    })
+}
+
+const showPic = async (ctx, next) => {
+    const fs = require('fs')
+    const path = require('path')
+    const picPath = path.resolve(__dirname,'../../../logs/img.jpeg')
+    ctx.type = 'image/jpeg'
+    const rs = fs.createReadStream(picPath)
+    ctx.body = rs
 }
 
 module.exports = {
     sp,
-    dytt
+    dytt,
+    download,
+    showPic,
 }
--- server/util/spider/index.js
+++ server/util/spider/index.js
@@ -1,12 +1,29 @@
 /**
  * Created by li on 2017/12/22 17:26.
  */
-const request = require('request-promise')
 const cheerio = require('cheerio')
+const rp = require('request-promise')
 const iconv = require('iconv-lite')
-const decodeType = "gbk"
-const {GET}  = require('../index')
-
+const defaultDecode = "utf8"
+const GET = async (uri,decodeType = defaultDecode ,isBuffer = false) => {
+    return new Promise(async (resolve,reject) => {
+        try{
+            const buffer = await rp({
+                uri,
+                encoding:null
+            })
+            if(isBuffer){
+                resolve(buffer)
+            } else {
+                const string = iconv.decode(buffer,decodeType)
+                resolve(string)
+            }
+        } catch(e){
+            console.log(e)
+            reject(false)
+        }
+    })
+}
 
 const get$ = async (url,decodeType="utf8") => {
     const htmlString = await GET(url,decodeType)
@@ -17,4 +34,5 @@ const get$ = async (url,decodeType="utf8") => {
 
 module.exports = {
     get$,
+    GET
 }
